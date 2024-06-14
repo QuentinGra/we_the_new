98bc1f93f1f157f58c33747a01be419bc7c986f0
QuentinGra <pilou4260042@gmail.com>
2024-06-14 14:19:14 +0200

    feat(stripe): add event stripe and webhook

--- src/Factory/StripeFactory.php
+++ src/Factory/StripeFactory.php
@@ -5,13 +5,21 @@ namespace App\Factory;
 use Stripe\Stripe;
 use App\Entity\Order\Order;
 use App\Entity\Order\OrderItem;
+use App\Event\StripeEvent;
 use Stripe\Checkout\Session;
+use Stripe\Event;
+use Stripe\Exception\SignatureVerificationException;
+use Stripe\Webhook;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 use Webmozart\Assert\Assert;
 
 class StripeFactory
 {
     public function __construct(
         private string $stripeSecretKey,
+        private string $webhookSecret,
+        private EventDispatcherInterface $eventDispatcher,
     ) {
         Stripe::setApiKey($stripeSecretKey);
         Stripe::setApiVersion('2024-04-10');
@@ -62,5 +70,64 @@ class StripeFactory
             ],
 
         ]);
+    }
+
+    /**
+     * Permet d'analyser la requête Stripe et de retourner l'évênement correspondant
+     *
+     * @param string $signature La signature de la requête
+     * @param mixed $body Le contenu de la requête
+     * @return JsonResponse une réponse JSON
+     */
+    public function handleStripeRequest(string $signature, mixed $body): JsonResponse
+    {
+        if (!$body) {
+            return new JsonResponse([
+                'status' => 'error',
+                'message' => 'Missing body content',
+            ], 404);
+        }
+
+        $event = $this->getEvent($signature, $body);
+
+        // Si Event est de la class JsonResponse, on retourne directement la réponse (Erreur)
+        if ($event instanceof JsonResponse) {
+            return $event;
+        }
+
+        $event = new StripeEvent($event);
+
+        $this->eventDispatcher->dispatch($event, $event->getName());
+
+        return new JsonResponse([
+            'status' => 'success',
+            'message' => 'Event received and processed successfully',
+        ]);
+    }
+
+    /**
+     * Permet de décoder la requête Stripe et de retourner l'évênement correspondant
+     *
+     * @param string $signature La signature de la requête
+     * @param mixed $body Le contenu de la requête
+     * @return Event|JsonResponse L'évênement Stripe ou une réponse JSON d'erreur
+     */
+    private function getEvent(string $signature, mixed $body): Event|JsonResponse
+    {
+        try {
+            $event = Webhook::constructEvent($body, $signature, $this->webhookSecret);
+        } catch (\UnexpectedValueException $e) {
+            return new JsonResponse([
+                'status' => 'error',
+                'message' => $e->getMessage(),
+            ], $e->getCode());
+        } catch (SignatureVerificationException $e) {
+            return new JsonResponse([
+                'status' => 'error',
+                'message' => $e->getMessage(),
+            ], $e->getCode());
+        }
+
+        return $event;
     }
 }
